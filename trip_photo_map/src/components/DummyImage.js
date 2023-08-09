c628c5b63badd30d540d2d78a509e48916fd3307
Philip Kwan <philipckwan@gmail.com>
2023-08-09 21:08:55 +0800

    v0.11 - cleanup

--- trip_photo_map/src/components/DummyImage.js
+++
@@ -1,9 +0,0 @@
-import Image from 'next/image';
-
-export function DummyImage() {
-  return (
-    <>
-      <Image src="/images/mac_background.png" alt="mac_background" width="1500" height="1500"/>
-    </>
-  );
-}
